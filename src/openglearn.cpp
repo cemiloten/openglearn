0c816b1bead36550d39e9713c2e73feda912a441
Cemil Oten <cemil.oten@gmail.com>
2019-03-13 18:16:50 +0100

    progress

--- src/openglearn.cpp
+++ src/openglearn.cpp
@@ -2,29 +2,35 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 
-const unsigned int windowWidth  = 800;
+const unsigned int windowWidth = 800;
 const unsigned int windowHeight = 600;
 
+void framebufferSizeCallback(GLFWwindow* window, const int width, const int height);
+void processInput(GLFWwindow* window);
+
+// todo:
+//    vertex array object
 int main()
 {
     if (!glfwInit()) {
         return -1;
     }
-    glfwWindowHint(GL_MAJOR_VERSION, 3);
-    glfwWindowHint(GL_MINOR_VERSION, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 #if defined __APPLE__
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 #endif
 
     // Window
-    GLFWwindow* window = glfwCreateWindow(windowWidth, windowHeight, "openGLearn", glfwGetPrimaryMonitor(), NULL);
+    GLFWwindow *window = glfwCreateWindow(windowWidth, windowHeight, "openGLearn", NULL, NULL);
     if (window == NULL) {
         printf("Error creating window\n");
         glfwTerminate();
         return -1;
     }
     glfwMakeContextCurrent(window);
+    glfwSetFramebufferSizeCallback(window, framebufferSizeCallback);
 
     // glad
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
@@ -32,6 +38,97 @@ int main()
         return -1;
     }
 
+    float vertices[9] {
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.0f,  0.5f, 0.0f
+    };
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    const char* vertexShaderSource =
+        "#version 330 core\n"
+        "layout (location = 0) in vec3 aPos;\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+        "}\n";
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+    glCompileShader(vertexShader);
+
+    const char* fragmentShaderSource =
+        "#version 330 core\n"
+        "out vec4 FragColor\n"
+        "\n"
+        "void main()\n"
+        "{\n"
+        "    gl_Position = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+        "}\n";
+    unsigned int fragmentShader;
+    fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+    glCompileShader(fragmentShader);
+
+    int success;
+    char infoLog[512];
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        printf("ERROR: Vertex shader compilation failed\n %s\n", infoLog);
+    }
+
+    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+    if (!success) {
+        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+        printf("ERROR: Fragment shader compilation failed\n %s\n", infoLog);
+    }
+
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragmentShader);
+    glLinkProgram(shaderProgram);
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+    if (!success) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        printf("ERROR: Shader program compilation failed\n %s\n", infoLog);
+    }
+
+    glUseProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragmentShader);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    // Main loop
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+        processInput(window);
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     printf("hellopengl\n");
     return 0;
+}
+
+void framebufferSizeCallback(GLFWwindow* window, const int width, const int height)
+{
+    // Match new dimensions
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
 }
