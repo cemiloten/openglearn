d27f74969571e2dc9239438d2b8a2ccbf0722713
Cemil Oten <cemil.oten@gmail.com>
2019-03-29 18:25:46 +0100

    remove App interface

--- src/openglearn.cpp
+++ src/openglearn.cpp
@@ -16,34 +16,113 @@
 #include "vao.h"
 #include "app.h"
 
-struct OpenGLearn : public IApp
+struct App
 {
-    OpenGLearn(const char* _name, const char* _description)
-        : IApp(_name, _description)
+    App(const char* _name, const char* _description)
+        : m_name(_name), m_description(_description)
     {}
 
-    virtual void initialize(
+    void initialize(
         const int _argc,
         const char* const* _argv,
         const unsigned int _width,
-        const unsigned int _height) override
+        const unsigned int _height)
     {
-        IApp::initialize(_argc, _argv, _width, _height);
+        m_width = _width;
+        m_height = _height;
 
         // TODO: initialize vertex data etc.
+        if (!glfwInit()) {
+            exit(1);
+        }
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+#if defined __APPLE__
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+
+        m_window = glfwCreateWindow(m_width, m_height, "openGLearn", NULL, NULL);
+        if (m_window == NULL) {
+            printf("Error creating window\n");
+            glfwTerminate();
+            exit(2);
+        }
+        glfwMakeContextCurrent(m_window);
+        glfwSetFramebufferSizeCallback(m_window, frame_buffer_size_callback);
+
+        // load OpenGL function pointers
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+            printf("Failed to initialize GLAD\n");
+            exit(3);
+        }
+
+        glEnable(GL_DEPTH_TEST);
+        glCullFace(GL_BACK);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 
-    virtual int shutdown() override
+    int shutdown()
     {
-        // free memory
+        // glDeleteVertexArrays(1, &cube);
+        // glDeleteBuffers(1, &vbo);
+        // glDeleteBuffers(1, &ebo);
+        // glfwTerminate();
     }
 
-    virtual bool update() override
+    bool update()
     {
+        while (!glfwWindowShouldClose(m_window))
+        {
+            process_input(m_window);
+
+            glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+            // glActiveTexture(GL_TEXTURE0);
+            // glBindTexture(GL_TEXTURE_2D, texture1);
+            // glActiveTexture(GL_TEXTURE1);
+            // glBindTexture(GL_TEXTURE_2D, texture2);
+
+            // // glm::mat4 view = cam.get_as_view_matrix();
+
+            // Shader::use(shader);
+            // // Shader::set_mat4(shader, "view",  view);
+            // Shader::set_mat4(shader, "proj",  proj);
+            // Shader::set_float(shader, "blending", 0.5f);
+
+            // VAO::bind(cube);
+            // VAO::draw(cube, *box);
+
+            // for (unsigned int i = 0; i < 10; ++i) {
+            //     glm::mat4 model = glm::mat4(1.0f);
+            //     model = glm::translate(model, cubePositions[i]);
+            //     if (i % 2 == 0) {
+            //         float angle = 20.0f * i;
+            //         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+            //     }
+            //     Shader::set_mat4(shader, "model", model);
+            //     glDrawArrays(GL_TRIANGLES, 0, 36);
+            // }
+            // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+            glfwSwapBuffers(m_window);
+            glfwPollEvents();
+        }
         // process events
         // draw stuff
         return false;
     }
+
+    const char* get_name() const { return m_name; }
+    const char* get_description() const { return m_description; }
+
+private:
+    const char* m_name;
+    const char* m_description;
+    unsigned int m_width;
+    unsigned int m_height;
+    GLFWwindow* m_window;
 };
 
 void frame_buffer_size_callback(GLFWwindow* window, const int width, const int height);
@@ -51,146 +130,43 @@ void process_input(GLFWwindow* window);
 
 int main()
 {
-    Mesh m;
-    ObjParser::parse("data/models/cube.obj", m);
 
-    // TODO: next
-    // GLuint vao = VAO::from_mesh(m);
-    std::cout << "ok" << std::endl;
-    return 0;
+    // unsigned int camera;
+    // if (!world.create_entity(camera)) {
+    //     return -1;
+    // }
+    // world.add_component(camera, ComponentType::Transform);
 
+    // std::vector<unsigned int> boxes(5);
+    // for (int i = 0; i < boxes.size(); ++i)
+    // {
+    //     if (!world.create_entity(boxes[i])) {
+    //         return -1;
+    //     }
+    //     world.add_components(
+    //         boxes[i],
+    //         { ComponentType::Transform, ComponentType::Mesh });
 
+    //    IComponent* c = world.get_component(boxes[i], ComponentType::Mesh);
+    //    c = box;
+    // }
 
+    // unsigned int shader = Shader::create(
+    //     "data/shaders/testShader.vert",
+    //     "data/shaders/testShader.frag");
 
+    // GLuint texture1 = Texture::load("data/textures/wall.jpg");
+    // GLuint texture2 = Texture::load("data/textures/smiley.png");
 
+    // Shader::use(shader);
+    // Shader::set_int(shader, "texture1", 0);
+    // Shader::set_int(shader, "texture2", 1);
 
+    // glm::mat4 proj = glm::mat4(1.0f);
+    // proj = glm::perspective(glm::radians(45.0f), (float)window_width / (float)window_height, 0.1f, 100.0f);
 
-
-    if (!glfwInit()) {
-        return -1;
-    }
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-#if defined __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
-
-    GLFWwindow* window = glfwCreateWindow(window_width, window_height, "openGLearn", NULL, NULL);
-    if (window == NULL) {
-        printf("Error creating window\n");
-        glfwTerminate();
-        return -1;
-    }
-    glfwMakeContextCurrent(window);
-    glfwSetFramebufferSizeCallback(window, frame_buffer_size_callback);
-
-    // load OpenGL function pointers
-    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-        printf("Failed to initialize GLAD\n");
-        return -1;
-    }
-
-    Mesh* box = new Mesh();
-    World world(10);
-
-    unsigned int camera;
-    if (!world.create_entity(camera)) {
-        return -1;
-    }
-    world.add_component(camera, ComponentType::Transform);
-
-    std::vector<unsigned int> boxes(5);
-    for (int i = 0; i < boxes.size(); ++i)
-    {
-        if (!world.create_entity(boxes[i])) {
-            return -1;
-        }
-        world.add_components(
-            boxes[i],
-            { ComponentType::Transform, ComponentType::Mesh });
-
-       IComponent* c = world.get_component(boxes[i], ComponentType::Mesh);
-       c = box;
-    }
-
-    unsigned int shader = Shader::create(
-        "data/shaders/testShader.vert",
-        "data/shaders/testShader.frag");
-
-    glEnable(GL_DEPTH_TEST);
-    glCullFace(GL_BACK);
-    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
-    int a = 0;
-
-    // glm::vec3 cubePositions[] = {
-    //     glm::vec3( 0.0f,  0.0f,  0.0f),
-    //     glm::vec3( 2.0f,  5.0f, -15.0f),
-    //     glm::vec3(-1.5f, -2.2f, -2.5f),
-    //     glm::vec3(-3.8f, -2.0f, -12.3f),
-    //     glm::vec3( 2.4f, -0.4f, -3.5f),
-    //     glm::vec3(-1.7f,  3.0f, -7.5f),
-    //     glm::vec3( 1.3f, -2.0f, -2.5f),
-    //     glm::vec3( 1.5f,  2.0f, -2.5f),
-    //     glm::vec3( 1.5f,  0.2f, -1.5f),
-    //     glm::vec3(-1.3f,  1.0f, -1.5f)
-    // };
-
-    GLuint texture1 = Texture::load("data/textures/wall.jpg");
-    GLuint texture2 = Texture::load("data/textures/smiley.png");
-
-    Shader::use(shader);
-    Shader::set_int(shader, "texture1", 0);
-    Shader::set_int(shader, "texture2", 1);
-
-    glm::mat4 proj = glm::mat4(1.0f);
-    proj = glm::perspective(glm::radians(45.0f), (float)window_width / (float)window_height, 0.1f, 100.0f);
-
-    GLuint cube = VAO::from_mesh(*box, VAO::DrawMode::Static);
-
-    while (!glfwWindowShouldClose(window)) {
-        process_input(window);
-
-        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-        glActiveTexture(GL_TEXTURE0);
-        glBindTexture(GL_TEXTURE_2D, texture1);
-        glActiveTexture(GL_TEXTURE1);
-        glBindTexture(GL_TEXTURE_2D, texture2);
-
-        // glm::mat4 view = cam.get_as_view_matrix();
-
-        Shader::use(shader);
-        // Shader::set_mat4(shader, "view",  view);
-        Shader::set_mat4(shader, "proj",  proj);
-        Shader::set_float(shader, "blending", 0.5f);
-
-        VAO::bind(cube);
-        VAO::draw(cube, *box);
-
-        // for (unsigned int i = 0; i < 10; ++i) {
-        //     glm::mat4 model = glm::mat4(1.0f);
-        //     model = glm::translate(model, cubePositions[i]);
-        //     if (i % 2 == 0) {
-        //         float angle = 20.0f * i;
-        //         model = glm::rotate(model, (float)glfwGetTime() * glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
-        //     }
-        //     Shader::set_mat4(shader, "model", model);
-        //     glDrawArrays(GL_TRIANGLES, 0, 36);
-        // }
-        // glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-        glfwSwapBuffers(window);
-        glfwPollEvents();
-    }
-
-    glDeleteVertexArrays(1, &cube);
-    glDeleteBuffers(1, &vbo);
-    glDeleteBuffers(1, &ebo);
-
-    glfwTerminate();
-    return 0;
+    // GLuint cube = VAO::from_mesh(*box, VAO::DrawMode::Static);
+    // return 0;
 }
 
 void frame_buffer_size_callback(GLFWwindow* window, const int width, const int height)
